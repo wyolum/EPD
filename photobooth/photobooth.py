158b4b249705129e8b161464de4c0cbea570017d
= <=>
2013-08-18 00:47:05 -0400

    photobooth.py added, still need to make gui

---
+++ photobooth/photobooth.py
@@ -0,0 +1,50 @@
+import os
+import os.path
+from custom_page import *
+import getpass
+
+SD_PATH = '/media/usb'
+SD_PATH = '.' 
+ALBUM = 'ALBUM'
+
+START_X = 175
+START_Y = 200
+W = 274
+H = 174
+
+HEADSHOT_WIDTH = 152
+HEADSHOT_HEIGHT = 128
+PAGE_BB = (START_X, START_Y, START_X + W, START_Y + H)
+HEADSHOT_BB = (START_X, START_Y, START_X + HEADSHOT_WIDTH, START_Y + HEADSHOT_HEIGHT)
+
+## ck if sd is present
+def sd_present():
+    return os.path.exists(SD_PATH)
+
+def sd_umount():
+    if sd_present():
+        call(['umount', SD_PATH])
+
+def ispi():
+    return getpass.getuser() == 'pi'
+
+def laptop_snap():
+    import pygame.camera
+    pygame.camera.init()
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+    cam.start()
+    img = cam.get_image()
+    imgstr = pygame.image.tostring(img, 'RGB')
+    im = Image.fromstring('RGB', img.get_size(), imgstr)
+    return im
+
+def snap(filename="photo.png", bb=HEADSHOT_BB):
+    if ispi():
+        im = pi_snap()
+    else:
+        im = laptop_snap()
+    im = im.crop(bb)
+    im.save(filename)
+    create_frontpage(SD_PATH, ALBUM, "person.csv", headshot=filename)
+
+snap()
